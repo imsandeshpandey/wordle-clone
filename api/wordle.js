8cbb26d3fb4bb00f4f2ca7e37b172a4c231beefa
Sandesh Pandey <sandeshpandeywork@gmail.com>
2024-07-05 19:52:59 +0545

    fix: minor typo fix in api handler

--- api/wordle.js
+++ api/wordle.js
@@ -1,5 +1,5 @@
-export default async function handler(_, res) {
-  if (res.method !== "GET") {
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
     res.status(405).json({ error: "Method not allowed" })
     return
   }
