16cd0eb1d77ad06dbb01f21eb693af36ce307ddb
Sandesh Pandey <sandeshpandeywork@gmail.com>
2024-08-17 15:23:33 +0545

    fix ui overflow on zoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { GridView } from "./components/grid-view-ref"
-import { KeyboardView } from "./components/keyboard-view"
+import { Keyboard } from "./components/keyboard-view"
 import { Results } from "./components/results"
 import { Button } from "./components/ui/button"
-import { Info, RefreshCwIcon } from "lucide-react"
-import { OnBoarding } from "./components/onboarding"
+import { RefreshCwIcon } from "lucide-react"
 import { ModeToggle } from "./components/theme-toggle"
 import { useWordle } from "./hooks/use-wordle.ts"
 import { FC } from "react"
 import { GameMode } from "./config/game.config"
 import { InvalidPathPage } from "./components/invalid-path-page.tsx"
 import { GameModeToggle } from "./components/game-mode-toggle.tsx"
-import { Branding } from "./components/branding.tsx"
 
 const pathname = window.location.pathname
 let gameMode: GameMode
@@ -45,45 +43,7 @@ export const App: FC = () => {
   }
 
   return (
-    <div className="relative mx-auto flex h-full max-h-[1080px] w-fit flex-col items-center justify-center gap-1 py-4">
-      <div>
-        <Branding className="mx-auto mb-8" />
-        <div className="flex justify-end">
-          <OnBoarding
-            trigger={
-              <Button
-                variant="ghost"
-                className="gap-1 text-xs text-muted-foreground"
-              >
-                <Info className="h-4 w-4" /> How To Play
-              </Button>
-            }
-          />
-        </div>
-        <GridView
-          update={rerenderDummyState}
-          refs={gridRefs}
-          grid={grid}
-          pos={[caretPosition.r, caretPosition.c]}
-        />
-      </div>
-      <div className="mt-4 w-fit">
-        <div className="mb-2 flex w-full items-center justify-around gap-2 rounded-full border px-4 text-xs font-bold md:text-sm">
-          <ModeToggle />
-          <span className="mx-2 h-6 w-[1px] bg-foreground/10" />
-          <GameModeToggle gameMode={gameMode} />
-          <span className="mx-2 h-6 w-[1px] bg-foreground/10" />
-          <Button
-            variant="ghost"
-            className="restart-button group -ml-4 gap-1 px-1 text-xs md:gap-2 md:text-sm"
-            onClick={restart}
-          >
-            <RefreshCwIcon className="rotate-in-90 h-3.5 w-3.5 animate-in md:h-4 md:w-4" />{" "}
-            Restart
-          </Button>
-        </div>
-        <KeyboardView keysStatus={keysStatus} onKeyInput={handleKeyInput} />
-      </div>
+    <div className="relative mx-auto flex h-full max-h-[1000px] w-fit flex-col items-center justify-center gap-1 py-8 pt-20">
       {isGameOver && (
         <Results
           gameMode={gameMode}
@@ -92,6 +52,40 @@ export const App: FC = () => {
           action={restart}
         />
       )}
+
+      <div className="relative flex w-full flex-1 p-2">
+        <div className="absolute inset-0 mx-auto mt-auto flex aspect-[5/6] max-h-full flex-col">
+          <GridView
+            update={rerenderDummyState}
+            refs={gridRefs}
+            grid={grid}
+            pos={[caretPosition.r, caretPosition.c]}
+          />
+        </div>
+      </div>
+      <footer className="mt-4 w-fit">
+        <Toolbar restart={restart} />
+        <Keyboard keysStatus={keysStatus} onKeyInput={handleKeyInput} />
+      </footer>
+    </div>
+  )
+}
+
+export const Toolbar = ({ restart }: { restart: () => void }) => {
+  return (
+    <div className="mb-2 flex w-full items-center justify-around gap-2 rounded-full border px-4 text-xs font-bold md:text-sm">
+      <ModeToggle />
+      <span className="mx-2 h-6 w-[1px] bg-foreground/10" />
+      <GameModeToggle gameMode={gameMode} />
+      <span className="mx-2 h-6 w-[1px] bg-foreground/10" />
+      <Button
+        variant="ghost"
+        className="restart-button group -ml-4 gap-1 px-1 text-xs md:gap-2 md:text-sm"
+        onClick={restart}
+      >
+        <RefreshCwIcon className="rotate-in-90 h-3.5 w-3.5 animate-in md:h-4 md:w-4" />{" "}
+        Restart
+      </Button>
     </div>
   )
 }
--- src/components/branding.tsx
+++ src/components/branding.tsx
@@ -4,14 +4,14 @@ import { cn } from "../lib/utils.ts"
 type BrandingProps = HTMLAttributes<HTMLDivElement>
 export const Branding: FC<BrandingProps> = ({ className, ...props }) => {
   return (
-    <div className={cn("w-fit py-2", className)} {...props}>
-      <h1 className="flex-items-center relative text-2xl font-bold drop-shadow-md md:text-4xl">
-        Wordle
-      </h1>
-
-      <p className="float-right ml-auto text-[8px] font-bold text-muted-foreground md:text-xs">
-        by frsty
-      </p>
-    </div>
+    <h1
+      className={cn(
+        "flex-items-center relative pb-3 text-2xl font-bold drop-shadow-md md:text-4xl",
+        className
+      )}
+      {...props}
+    >
+      Wordle
+    </h1>
   )
 }
